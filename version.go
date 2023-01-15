fae49ea4102f25219d0d11109a19d25f66212d2b
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2023-01-15 04:25:00 -0800

    v1.0.1 release

--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package main
 
 const (
-	Version     = "v1.0.0"
-	GitCommit   = "402ccb3"          // the commit JUST BEFORE the release
-	VersionDate = "2023-01-15 12:13" // UTC
+	Version     = "v1.0.1"
+	GitCommit   = "34f34ae"          // the commit JUST BEFORE the release
+	VersionDate = "2023-01-15 12:25" // UTC
 )
