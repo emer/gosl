bced5724e1f73c9814a84b3500ee3cf0368c109f
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-01-25 14:57:22 -0800

    updated version to v2.0.0-dev0.0.10

--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package main
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.9"
+	Version = "v2.0.0-dev0.0.10"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "9ddd079"
+	GitCommit = "5668959"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2024-01-18 00:59"
+	VersionDate = "2024-01-25 22:57"
 )
